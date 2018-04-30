36a4f029cfd4404601e275f0f6d1dbecb317c190
dks0462720 <dks0462720@WSHX000229>
2018-04-30 15:32:24 -0400

    adding implementation code tests

---
+++ implement-code/lib/parser.rb
@@ -0,0 +1,10 @@
+class Parser
+    attr_accessor :final_data
+    def initialize(value)
+        @final_data = []
+        parse_data(value)
+    end
+    private def parse_data(value)
+        #implement Code Here
+    end
+end
---
+++ implement-code/spec/parser_spec.rb
@@ -0,0 +1,72 @@
+require 'parser'
+
+describe Parser do
+    describe ".initialize" do
+        context "given the string '~fOo'" do
+            it "should add 'FoO' to the final_data array" do
+                parser = Parser.new("~fOo")
+                expect(parser.final_data).to be_include("FoO")
+            end
+        end
+        
+        context "given the string '~FOo'" do
+            it "should add 'foO' to the final_data array" do
+                parser = Parser.new("~FOo")
+                expect(parser.final_data).to be_include("foO")
+            end
+        end
+        context "given the string '>FOO'" do
+            it "should add 'foo' to the final_data array" do
+                parser = Parser.new(">FOO")
+                expect(parser.final_data).to be_include("foo")
+            end
+        end
+        context "given the string '>FOOBAR'" do
+            it "should add 'foobar' to the final_data array" do
+                parser = Parser.new(">FOOBAR")
+                expect(parser.final_data).to be_include("foobar")
+            end
+        end
+        context "given the string '<foo'" do
+            it "should add 'FOO' to the final_data array" do
+                parser = Parser.new("<foo")
+                expect(parser.final_data).to be_include("FOO")
+            end
+        end
+        context "given the string '<foobar'" do
+            it "should add 'FOOBAR' to the final_data array" do
+                parser = Parser.new("<foobar")
+                expect(parser.final_data).to be_include("FOOBAR")
+            end
+        end
+        context "given the string '-foo'" do
+            it "should add 'f' to the final_data array" do
+                parser = Parser.new("-foo")
+                expect(parser.final_data).to be_include("f")
+            end
+        end
+        context "given the string '-foobar'" do
+            it "should add 'f' to the final_data array" do
+                parser = Parser.new("-foobar")
+                expect(parser.final_data).to be_include("f")
+            end
+        end
+    end
+    describe "Error handling" do
+        context "given the string '+foo'" do
+            it "should raise 'format error'" do
+                expect{Parser.new("+foo")}.to raise_error("format error")
+            end
+        end
+        context "given the string '*foobar'" do
+            it "should raise 'format error'" do
+                expect{Parser.new("*foobar")}.to raise_error("format error")
+            end
+        end
+        context "given the string '*foobar'" do
+            it "should raise 'format error'" do
+                expect{Parser.new("*foobar")}.to raise_error("format error")
+            end
+        end
+    end
+end
