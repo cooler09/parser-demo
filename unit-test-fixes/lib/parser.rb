0afe2e150e6525bb4a7e73a198c01e1ee5945148
dks0462720 <dks0462720@WSHX000229>
2018-04-30 13:22:57 -0400

    adding more broken tests

--- unit-test-fixes/lib/parser.rb
+++ unit-test-fixes/lib/parser.rb
@@ -5,7 +5,11 @@ class Parser
             raise "Please enter a string to parse"
         end
         if value.match(/pipe:/)
+            value.sub!("pipe:", "")
             value.split("|")
+        elsif value.match(/comma:/)
+            value.sub!("comma:", "")
+            value.split(",")
         else
             value.split(",")
         end
---
+++ unit-test-fixes/spec/parser_spec.rb
@@ -0,0 +1,45 @@
+require 'parser'
+
+describe Parser do
+    before do
+        @parser = Parser.new
+    end
+    describe ".parse" do
+        context "given the number 3" do
+            it "should raise error" do
+                expect{@parser.parse(3)}.to raise_error("Please enter a string to parse")
+#                expect{@parser.parse("3")}.to raise_error("Please enter a string to parse")
+            end
+        end
+        context "given the value 'foo,bar'" do
+            it "should return ['foo','bar']" do
+                expect(@parser.parse("foo,bar")).to eql(['foo','bar'])
+                #expect(@parser.parse("foo,bar")).to eql(['bar','foo'])
+            end
+        end
+        context "given the string 'pipe:foo|bar'" do
+            it "should return ['foo','bar']" do
+                expect(@parser.parse("pipe:foo|bar")).to eql(['foo','bar'])
+                #expect(@parser.parse("pipe:foo|bar")).to be(['foo','bar'])
+            end
+        end
+        context "given the string 'comma:foo,bar'" do
+            it "should return ['foo','bar']" do
+                expect(@parser.parse("comma:foo,bar")).to eql(['foo','bar'])
+                #expect(@parser.parse("pipe:foo,bar")).not_to eql(['foo','bar'])
+            end
+        end
+        context "given the string 'pipe:foo,bar'" do
+            it "should return ['foo,bar']" do
+                expect(@parser.parse("pipe:foo,bar")).to eql(['foo,bar'])
+                #expect(@parser.parse("pipe:foo|bar")).to eql(['foo,bar'])
+            end
+        end
+        context "given the string 'pipe:foo|bar|3'" do
+            it "should return ['foo','bar','3']" do
+                expect(@parser.parse("pipe:foo|bar|3")).to eql(['foo','bar','3'])
+                #expect(@parser.parse("pipe:foo|bar|3")).to eql(['foo','bar',3])
+            end
+        end
+    end
+end
