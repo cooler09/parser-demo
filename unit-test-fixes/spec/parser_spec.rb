4cc3d3a7ff1990ab91b30e80141a18af59482ef1
Jean Lange <langeje@us.ibm.com>
2018-05-05 09:39:38 -0400

    fix-tests: reorganize tests and add a few

--- unit-test-fixes/spec/parser_spec.rb
+++ unit-test-fixes/spec/parser_spec.rb
@@ -6,25 +6,37 @@ describe Parser do
     end
 
     describe ".parse" do
-        context "error conditions" do
-            it "only parses strings, not numbers" do
-                expect{@parser.parse("3")}.to raise_error("Please enter a string to parse")
-            end
-        end
-
-        context "without a specified separator" do
+        context "basic functionality" do
             it "separates by comma and returns an array of strings" do
                 expect(@parser.parse("foo,bar")).to eql(["bar","foo"])
             end
+
+            it "deals with only one input element" do
+                expect(@parser.parse("foo")).to eql(["bar"])
+            end
+
+            it "deals with many input elements" do
+                expect(@parser.parse("foo|bar|baz|3")).to eql(["foo", "bar", "baz", 3])
+            end
+
+            it "leaves input elements in the same order" do
+                parsed = @parser.parse("foo,bar")
+                expect(parsed[0]).to eql("bar")
+                expect(parsed[1]).to eql("bar")
+            end
         end
 
-        context "when the separator is specified" do
+        context "with a named separator" do
             it "can use pipe as a separator" do
                 expect(@parser.parse("pipe:foo|bar")).to be(["foo","bar"])
             end
 
             it "can use comma as an explicit separator" do
                 expect(@parser.parse("pipe:foo,bar")).to eql(["foo","bar"])
+            end
+
+            it "doesn't work with separators it doesn't understand" do
+                expect(@parser.parse("star:foo*bar")).to eql(["foo", "bar"])
             end
         end
 
@@ -38,9 +50,9 @@ describe Parser do
             end
         end
 
-        context "with more than two elements" do
-            it "returns them all as strings" do
-                expect(@parser.parse("pipe:foo|bar|3")).to eql(["foo","bar",3])
+        context "error conditions" do
+            it "only parses strings, not numbers" do
+                expect{@parser.parse("3")}.to raise_error("Please enter a string to parse")
             end
         end
     end
